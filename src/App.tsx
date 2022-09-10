1da71024d4cc0ff9b04eaea46e68208629ef4be9
Javokhir Rajabboev <javokhir_rajabboev@epam.com>
2022-09-10 22:08:51 +0500

    Implement modal functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Board from './components/Board/Board';
 import Header from './components/Header/Header';
+import Modal from './components/Modal/Modal';
 
 function App() {
   return (
     <>
       <Header />
       <Board />
+      <Modal opened={true}>!!!Modal!!!</Modal>
     </>
   );
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,33 @@
-function Modal() {
-  return <div>Modal</div>;
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+export interface ModalProps {
+  children: ReactNode;
+  opened: boolean;
+  target?: HTMLElement | string;
+}
+
+function Modal({ children, opened, target = document.body }: ModalProps) {
+  const [mounted, setMounted] = useState(false);
+  const ref = useRef<HTMLElement>();
+
+  useEffect(() => {
+    setMounted(true);
+
+    ref.current =
+      typeof target === 'string'
+        ? (document.querySelector(target) as HTMLElement)
+        : target;
+  }, [target]);
+
+  if (!opened || !mounted || !ref.current) return null;
+
+  return createPortal(
+    <div className="absolute flex items-center justify-center w-full h-full bg-transparent top-0 left-0">
+      <div className="w-60 h-60 bg-indigo-300">{children}</div>
+    </div>,
+    ref.current
+  );
 }
 
 export default Modal;
